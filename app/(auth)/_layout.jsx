943363b267c871b298ac356095fa97cf919831d7
bombh <fabrice@bxxl.be>
2024-07-16 16:40:51 +0200

    SignIn & SignUp screens

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
 
 const AuthLayout = () => {
    return (
-      <View>
-         <Text>AuthLayout</Text>
-      </View>
+      <>
+         <StatusBar
+            backgroundColor="#161622"
+            style="light"
+         />
+
+         <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="sign-in" />
+            <Stack.Screen name="sign-up" />
+         </Stack>
+      </>
    )
 }
 
---
+++ src/components/FormField.jsx
@@ -0,0 +1,41 @@
+import { View, Text, TextInput, Pressable, Image } from "react-native"
+import React, { useState } from "react"
+
+import { icons } from "@/constants"
+
+const FormField = ({ title, value, handeChange, containerStyles, placeholder, ...props }) => {
+   const [showPassword, setShowPassword] = useState(false)
+
+   return (
+      <View className={`space-y-2 ${containerStyles}`}>
+         <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+         <View className="flex-row w-full h-16 px-4 items-center rounded-xl bg-black-100 border border-black-200 focus:border-secondary">
+            <TextInput
+               value={value}
+               onChangeText={handeChange}
+               placeholder={placeholder}
+               placeholderTextColor="#7B7B8B"
+               className="flex-1 text-white text-base font-psemibold"
+               secureTextEntry={title === "Password" && !showPassword}
+               {...props}
+            />
+
+            {title === "Password" && (
+               <Pressable
+                  onPress={() => setShowPassword(!showPassword)}
+                  className="active:opacity-70"
+               >
+                  <Image
+                     source={!showPassword ? icons.eye : icons.eyeHide}
+                     className="w-6 h-6"
+                     resizeMode="contain"
+                  />
+               </Pressable>
+            )}
+         </View>
+      </View>
+   )
+}
+
+export default FormField
