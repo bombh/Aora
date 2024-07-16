a9f9d018ae4bd116a9db037045df26056f079506
bombh <fabrice@bxxl.be>
2024-07-16 13:27:39 +0200

    Route structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
@@ -5,6 +6,12 @@ export default function App() {
    return (
       <View className="flex-1 items-center justify-center">
          <Text className="text-3xl font-pblack font-bold">Aora!</Text>
+         <Link
+            href="/home"
+            className="text-blue-500"
+         >
+            Go to Home
+         </Link>
       </View>
    )
 }
