04986ea52a7af7160d7bc4c70f8ce5c6e956284e
bombh <fabrice@bxxl.be>
2024-07-16 13:11:07 +0200

    Base

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+
+export default function App() {
+   return (
+      <View className="flex-1 items-center justify-center">
+         <Text className="text-3xl font-pblack font-bold">Aora!</Text>
+      </View>
+   )
+}
